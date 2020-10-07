51562684cd91eacda9de82392e8c458d657e1415
Will McKinley <willowmck@gmail.com>
2020-10-07 18:19:49 -0400

    Add pattern for installing/updating EgressNetworkPolicy

---
+++ upgrade-egress-policies.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Define these variables in the same shell
+DEPLOY_NAMESPACE=${bookinfo_namespace}
+
+echo "Install EgressNetworkPolicy in namespace \"${DEPLOY_NAMESPACE}\""
+
+helm upgrade --install egress-policies -n ${DEPLOY_NAMESPACE} egress-policies --values values/egress-values.yaml
+
+echo "Done."
+
+exit 0
